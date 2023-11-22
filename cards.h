2fdf43a8414c762f889ac435077b21b24d14d2f3
johnhagedorncs <johnhagedorn@ucsb.edu>
2023-11-21 20:28:39 -0800

    bruh

--- cards.h
+++ cards.h
@@ -1,8 +1,90 @@
 // cards.h
-// Author: Your name
+// Author: John Hagedorn
 // All class declarations go here
 
-#ifndef CARDS_H
-#define CARDS_H
+#define UTILITY_H
+#include "utility.h"
+#include <string>
+#include <string>
+#include <iostream>
 
-#endif
+using namespace std;
+
+struct Node {
+    int suit;
+    int value;
+    Node* left;
+    Node* right;
+    Node* parent;
+
+    Node(int suitNum = 0, int valueNum = 0) {
+        suit = suitNum;
+        value = valueNum;
+        right = left = parent = nullptr;
+    }
+};
+
+class CardBST {
+public:
+    CardBST();
+    ~CardBST();
+    void printInOrder();
+    void printInOrder(Node* n);
+    void clear(Node *n);
+    void printPredecessor(int val1, int val2);
+    void printSuccessor(int val1, int val2);
+    void removeNode(int val1, int val2);
+    Node* remove(int val1, int val2, Node* n);
+    bool has(int val1, int val2);
+    bool insert(int val1, int val2);
+    bool insert(int val1, int val2, Node*& n);
+    Node* getMin() {
+        return min(root);
+    };
+    Node* getMax() {
+        return max(root);
+    };
+    Node* successor(int val1, int val2);
+    Node* successor(int val1, int val2, Node* n);
+    Node* predecessor(int val1, int val2);
+    Node* predecessor(int val1, int val2, Node* n);
+    Node* getNode(int val1, int val2, Node* n);
+    string suitToString (int suitValue);
+    string valToString (int valueNum);
+
+
+private:
+    Node* root;
+    Node* min(Node *n);
+    Node* max(Node *n);
+    bool nodeFinder(int val1, int val2, Node* n);
+};
+
+class Card{
+    public:
+    int suit;
+    int value;
+    Card(int suitNum = 0, int valueNum = 0) : suit(suitNum), value(valueNum) {}
+    
+    bool operator==(const Card& other) const {
+        return (suit == other.suit) && (value == other.value);
+    }
+    bool operator<(const Card& other) const {
+        if (suit == other.suit) {
+            return value < other.value;
+        }
+        return suit < other.suit;
+    }
+    bool operator>(const Card& other) const {
+        if (suit == other.suit) {
+            return value > other.value;
+        }
+        return suit > other.suit;
+    }
+    bool operator<=(const Card& other) const {
+        return (value < other.value) || (value == other.value && suit <= other.suit);
+    }
+    bool operator>=(const Card& other) const {
+        return (value > other.value) || (value == other.value && suit >= other.suit);
+    }
+};
