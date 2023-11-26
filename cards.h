fa549175f601dd5473204a695f5f77e837443a6e
johnhagedorncs <johnhagedorn@ucsb.edu>
2023-11-26 11:33:11 -0800

    bruh

--- cards.h
+++ cards.h
@@ -7,6 +7,7 @@
 #include <string>
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -20,7 +21,9 @@ struct Node {
     Node(int suitNum = 0, int valueNum = 0) {
         suit = suitNum;
         value = valueNum;
-        right = left = parent = nullptr;
+        right = nullptr;
+        left = nullptr;
+        parent = nullptr;
     }
 };
 
@@ -28,63 +31,31 @@ class CardBST {
 public:
     CardBST();
     ~CardBST();
+    bool find(int val1, int val2);
+    void removeNode(int val1, int val2);
+    bool insert(int val1, int val2);
+    Node* succ(int val1, int val2);
+    Node* pred(int val1, int val2);
     void printInOrder();
     void printInOrder(Node* n);
-    void clear(Node *n);
-    void printPredecessor(int val1, int val2);
-    void printSuccessor(int val1, int val2);
-    void removeNode(int val1, int val2);
-    Node* remove(int val1, int val2, Node* n);
-    bool has(int val1, int val2);
-    bool insert(int val1, int val2);
-    bool insert(int val1, int val2, Node*& n);
+    string valToString (int valueNum);
+    string suitToString (int suitValue);
+    void printPred(int val1, int val2);
+    void printSucc(int val1, int val2);
+    void clearNode(Node *n);
+    Node* getNode(int val1, int val2, Node* n);
     Node* getMin() {
         return min(root);
     };
     Node* getMax() {
         return max(root);
     };
-    Node* successor(int val1, int val2);
-    Node* successor(int val1, int val2, Node* n);
-    Node* predecessor(int val1, int val2);
-    Node* predecessor(int val1, int val2, Node* n);
-    Node* getNode(int val1, int val2, Node* n);
-    string suitToString (int suitValue);
-    string valToString (int valueNum);
-
 
 private:
     Node* root;
     Node* min(Node *n);
     Node* max(Node *n);
-    bool nodeFinder(int val1, int val2, Node* n);
-};
-
-class Card{
-    public:
-    int suit;
-    int value;
-    Card(int suitNum = 0, int valueNum = 0) : suit(suitNum), value(valueNum) {}
-    
-    bool operator==(const Card& other) const {
-        return (suit == other.suit) && (value == other.value);
-    }
-    bool operator<(const Card& other) const {
-        if (suit == other.suit) {
-            return value < other.value;
-        }
-        return suit < other.suit;
-    }
-    bool operator>(const Card& other) const {
-        if (suit == other.suit) {
-            return value > other.value;
-        }
-        return suit > other.suit;
-    }
-    bool operator<=(const Card& other) const {
-        return (value < other.value) || (value == other.value && suit <= other.suit);
-    }
-    bool operator>=(const Card& other) const {
-        return (value > other.value) || (value == other.value && suit >= other.suit);
-    }
+    Node* remove(int val1, int val2, Node* n);
+    bool insert(int val1, int val2, Node* n);
+    bool findNode(int val1, int val2, Node* n);
 };
--- utility.h
+++ utility.h
@@ -1,13 +1,1 @@
 // CardUtility.h
-#ifndef CARD_UTILITY_H
-#define CARD_UTILITY_H
-
-#include <string>
-
-class CardUtility {
-public:
-    static std::string suitToString(int suit);
-    static std::string valToString(int value);
-};
-
-#endif  // CARD_UTILITY_H
