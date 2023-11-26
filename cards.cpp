e8c2be4c7ca3bb93ae589d5dba58d2fd4bbc63eb
johnhagedorncs <johnhagedorn@ucsb.edu>
2023-11-26 11:32:14 -0800

    bru

--- cards.cpp
+++ cards.cpp
@@ -1,356 +1,242 @@
-#include "cards.h"
-#include <fstream>
-#include <limits>
-
-// Implementation for Card class
-bool Card::operator==(const Card& other) const {
-    return (suit == other.suit) && (value == other.value);
-}
-
-bool Card::operator<(const Card& other) const {
-    if (suit != other.suit) {
-        return suit < other.suit;
-    }
-    return value < other.value;
-}
-
-std::ostream& operator<<(std::ostream& os, const Card& card) {
-    os << card.suit << " " << card.value;
-    return os;
-}
-
-// Implementation for BST class
-BST::Node::Node(const Card& card) : data(card), left(nullptr), right(nullptr) {}
-
-BST::BST() : root(nullptr) {}
-
-void BST::insert(const Card& card) {
-    root = insertHelper(root, card);
-}
-
-BST::Node* BST::insertHelper(Node* node, const Card& card) {
-    if (node == nullptr) {
-        return new Node(card);
-    }
-
-    if (card < node->data) {
-        node->left = insertHelper(node->left, card);
-    } else {
-        node->right = insertHelper(node->right, card);
-    }
-
-    return node;
-}
-
-std::vector<Card> BST::inOrderTraversal() const {
-    std::vector<Card> result;
-    inOrderTraversalHelper(root, result);
-    return result;
-}
-
-void BST::inOrderTraversalHelper(Node* node, std::vector<Card>& result) const {
-    if (node) {
-        inOrderTraversalHelper(node->left, result);
-        result.push_back(node->data);
-        inOrderTraversalHelper(node->right, result);
-    }
-}
-
-// Function to read cards from a file and insert them into a BST
-void readCards(std::ifstream& file, BST& tree) {
-    char suit;
-    std::string value;
-
-    while (file.get(suit) >> value) {
-        file.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the rest of the line
-        Card card{suit, value};
-        tree.insert(card);
-    }
-}
-/*
 // cards.cpp
 // Author: John Hagedorn
-// Implementation of the CardBST class
+// Implementation of the functions for the CardBST class
 
 #include "cards.h"
 #include <iostream>
 
 using namespace std;
 
-// Constructor
-CardBST::CardBST() : root(nullptr) {}
+CardBST::CardBST() : root(nullptr) {}       // constructor function
 
-// Destructor
-CardBST::~CardBST() {
-    clear(root);
+CardBST::~CardBST() {                       // destructor function
+    clearNode(root);
 }
 
-// Function to print the cards in order
-void CardBST::printInOrder() {
-    printInOrder(root);
+bool CardBST::insert(int val1, int val2) {  // insert function
+    return insert(val1, val2, root);
 }
 
-// Function to clear the tree
-void CardBST::clear(Node* n) {
-    if (n != nullptr) {
-        clear(n->left);
-        clear(n->right);
-        delete n;
-    }
+bool CardBST::find(int val1, int val2) {     // function to find
+    return findNode(val1, val2, root);
 }
 
-// Function to print the predecessor of a card in the BST
-void CardBST::printPredecessor(int val1, int val2) {
-    Node* predecessor = this->predecessor(val1, val2);
-    if (predecessor != nullptr) {
-        cout << "Predecessor: " << suitToString(predecessor->suit) << " " << valToString(predecessor->value) << endl;
-    } else {
-        cout << "No predecessor found." << endl;
-    }
-}
-
-// Function to print the successor of a card in the BST
-void CardBST::printSuccessor(int val1, int val2) {
-    Node* successor = this->successor(val1, val2);
-    if (successor != nullptr) {
-        cout << "Successor: " << suitToString(successor->suit) << " " << valToString(successor->value) << endl;
-    } else {
-        cout << "No successor found." << endl;
-    }
-}
-
-// Function to remove a card from the BST
-void CardBST::removeNode(int val1, int val2) {
+void CardBST::removeNode(int val1, int val2) {      // remove function
     root = remove(val1, val2, root);
 }
 
-// Recursive helper function to remove a card from the BST
-Node* CardBST::remove(int val1, int val2, Node* n) {
-    if (n == nullptr) {
-        return nullptr;
+Node* CardBST::succ(int val1, int val2) {
+    Node* node = getNode(val1, val2, root);
+    if (!node) return nullptr;
+
+    if (node->right) {  // If node has right subtree, find min in subtree
+        return min(node->right);
     }
 
-    Card currentCard(n->suit, n->value);
-    Card targetCard(val1, val2);
+    Node* successor = nullptr;  // else find nearest node for given node in the left subtree
+    Node* current = root;
 
-    if (currentCard < targetCard) {
-        n->right = remove(val1, val2, n->right);
-    } else if (currentCard > targetCard) {
-        n->left = remove(val1, val2, n->left);
-    } else {
-        // Node with only one child or no child
-        if (n->left == nullptr) {
-            Node* temp = n->right;
-            delete n;
-            return temp;
-        } else if (n->right == nullptr) {
-            Node* temp = n->left;
-            delete n;
-            return temp;
-        }
-
-        // Node with two children, get the inorder successor
-        Node* successor = min(n->right);
-
-        // Copy the inorder successor's data to this node
-        n->suit = successor->suit;
-        n->value = successor->value;
-
-        // Delete the inorder successor
-        n->right = remove(successor->suit, successor->value, n->right);
-    }
-    return n;
-}
-
-// Function to check if a card is present in the BST
-bool CardBST::has(int val1, int val2) {
-    return nodeFinder(val1, val2, root);
-}
-
-// Function to get the successor of a card in the BST
-Node* CardBST::successor(int val1, int val2) {
-    return successor(val1, val2, root);
-}
-
-// Function to get the predecessor of a card in the BST
-Node* CardBST::predecessor(int val1, int val2) {
-    return predecessor(val1, val2, root);
-}
-
-// Recursive helper function to find the successor of a card in the BST
-Node* CardBST::successor(int val1, int val2, Node* n) {
-    if (n == nullptr) {
-        return nullptr;
-    }
-
-    Card targetCard(val1, val2);
-
-    Node* successorNode = nullptr;
-    while (n != nullptr) {
-        Card currentCard(n->suit, n->value);
-
-        if (currentCard <= targetCard) {
-            n = n->right;
+    while (current) {
+        if (val1 < current->suit || (val1 == current->suit && val2 < current->value)) {
+            successor = current;
+            current = current->left;
+        } else if (val1 > current->suit || (val1 == current->suit && val2 > current->value)) {
+            current = current->right;
         } else {
-            successorNode = n;
-            n = n->left;
+            break; // found node
         }
     }
 
-    return successorNode;
+    return successor;
 }
 
-// Recursive helper function to find the predecessor of a card in the BST
-Node* CardBST::predecessor(int val1, int val2, Node* n) {
-    if (n == nullptr) {
-        return nullptr;
+Node* CardBST::pred(int val1, int val2) {
+    Node* node = getNode(val1, val2, root);
+    if (!node) return nullptr;
+
+    if (node->left) {       // If node has left subtree, find max in subtree
+        return max(node->left);
     }
 
-    Card currentCard(n->suit, n->value);
-    Card targetCard(val1, val2);
+    Node* predecessor = nullptr;    // else find nearest node for given node in the right subtree
+    Node* current = root;
 
-    if (currentCard >= targetCard) {
-        return predecessor(val1, val2, n->left);
-    } else {
-        Node* predecessorNode = predecessor(val1, val2, n->right);
-        return (predecessorNode != nullptr) ? predecessorNode : n;
+    while (current) {
+        if (val1 < current->suit || (val1 == current->suit && val2 < current->value)) {
+            current = current->left;
+        } else if (val1 > current->suit || (val1 == current->suit && val2 > current->value)) {
+            predecessor = current;
+            current = current->right;
+        } else {
+            break; // found node
+        }
     }
+
+    return predecessor;
 }
 
-// Function to get a specific node in the BST
-Node* CardBST::getNode(int val1, int val2, Node* n) {
-    if (n == nullptr) {
-        return nullptr;
-    }
-
-    Card currentCard(n->suit, n->value);
-    Card targetCard(val1, val2);
-
-    if (currentCard == targetCard) {
-        return n;
-    } else if (currentCard < targetCard) {
-        return getNode(val1, val2, n->right);
-    } else {
-        return getNode(val1, val2, n->left);
-    }
+void CardBST::printInOrder() {  // printInOrder function
+    printInOrder(root);
 }
 
-// Utility function to convert suit to string
-string CardBST::suitToString(int suit) {
-    switch (suit) {
-        case 1: return "c";
-        case 2: return "d";
-        case 3: return "s";
-        case 4: return "h";
-        default: return "Unknown";
-    }
-}
-
-// Utility function to convert value to string
-string CardBST::valToString(int value) {
-    if (value == 1) {
-        return "a";
-    } else if (value == 11) {
-        return "j";
-    } else if (value == 12) {
-        return "q";
-    } else if (value == 13) {
-        return "k";
-    } else {
-        return to_string(value);
-    }
-}
-
-// Recursive helper function to print the cards in order
-void CardBST::printInOrder(Node* n) {
-    if (n != nullptr) {
+void CardBST::printInOrder(Node* n) {   // printInOrder helper function
+    if (n) {
         printInOrder(n->left);
-        cout << suitToString(n->suit) << " " << valToString(n->value) << endl;
+        cout << suitToString(n->suit) << " " << valToString(n->value) << std::endl;
         printInOrder(n->right);
     }
 }
 
-// Recursive helper function to find the minimum value node in the BST
-Node* CardBST::min(Node* n) {
-    while (n->left != nullptr) {
+string CardBST::suitToString(int suitValue) {   // function converts card suit to string
+    switch (suitValue) {
+        case 1:
+            return "c";
+        case 2:
+            return "d";
+        case 3:
+            return "s";
+        case 4:
+            return "h";
+        default:
+            return "";
+    }
+}
+
+string CardBST::valToString(int valueNum) {     // function converts card value to string
+    switch (valueNum) {
+        case 1:
+            return "a";
+        case 11:
+            return "j";
+        case 12:
+            return "q";
+        case 13:
+            return "k";
+        default:
+            return to_string(valueNum);
+    }
+}
+
+void CardBST::clearNode(Node* n) {              // function to clear given node
+    if (n) {
+        clearNode(n->left);
+        clearNode(n->right);
+        delete n;
+    }
+}
+
+void CardBST::printPred(int val1, int val2) {    // cout predecessor function
+    Node* pred1 = pred(val1, val2);
+    if (pred1) {
+        cout << "Predecessor: " << suitToString(pred1->suit) << " " << valToString(pred1->value) << std::endl;
+    } else {
+        cout << "No predecessor found." << std::endl;
+    }
+}
+
+void CardBST::printSucc(int val1, int val2) {      // cout successor function
+    Node* succ1 = succ(val1, val2);
+    if (succ1) {
+        cout << "Successor: " << suitToString(succ1->suit) << " " << valToString(succ1->value) << std::endl;
+    } else {
+        cout << "No successor found." << std::endl;
+    }
+}
+
+Node* CardBST::getNode(int val1, int val2, Node* n) {    // function gets node given val
+    if (!n) {
+        return nullptr;
+    }
+
+    if (val1 < n->suit || (val1 == n->suit && val2 < n->value)) {
+        return getNode(val1, val2, n->left);
+    } else if (val1 > n->suit || (val1 == n->suit && val2 > n->value)) {
+        return getNode(val1, val2, n->right);
+    } else {
+        return n;
+    }
+}
+
+Node* CardBST::min(Node* n) {           // function finds min node
+    while (n && n->left) {
         n = n->left;
     }
     return n;
 }
 
-// Recursive helper function to find the maximum value node in the BST
-Node* CardBST::max(Node* n) {
-    while (n->right != nullptr) {
+Node* CardBST::max(Node* n) {           // function finds max node
+    while (n && n->right) {
         n = n->right;
     }
     return n;
 }
 
-bool CardBST::insert(int val1, int val2) {
-    return insert(val1, val2, root);
-}
-
-// Recursive helper function to insert a card into the BST
-bool CardBST::insert(int val1, int val2, Node*& n) {
-    if (n == nullptr) {
-        n = new Node(val1, val2);
+bool CardBST::insert(int val1, int val2, Node* n) {     // insert helper function
+    if (!n) {
+        root = new Node(val1, val2);
         return true;
     }
 
-    Card currentCard(n->suit, n->value);
-    Card targetCard(val1, val2);
-
-    if (currentCard < targetCard) {
-        if (n->right == nullptr) {
-            n->right = new Node(val1, val2);
-            n->right->parent = n;  // Update the parent pointer
-            return true;
-        } else {
-            return insert(val1, val2, n->right);
-        }
-    } else if (currentCard > targetCard) {
-        if (n->left == nullptr) {
-            n->left = new Node(val1, val2);
-            n->left->parent = n;  // Update the parent pointer
-            return true;
-        } else {
+    if (val1 < n->suit || (val1 == n->suit && val2 < n->value)) {
+        if (n->left) {
             return insert(val1, val2, n->left);
+        } else {
+            n->left = new Node(val1, val2);
+            n->left->parent = n;
+            return true;
+        }
+    } else if (val1 > n->suit || (val1 == n->suit && val2 > n->value)) {
+        if (n->right) {
+            return insert(val1, val2, n->right);
+        } else {
+            n->right = new Node(val1, val2);
+            n->right->parent = n;
+            return true;
         }
     } else {
-        // Duplicate card found
-        return false;
+        return false; // duplicate
     }
 }
 
-// Recursive helper function to check if a card is present in the BST
-bool CardBST::nodeFinder(int val1, int val2, Node* n) {
-    if (n == nullptr) {
+bool CardBST::findNode(int val1, int val2, Node* n) {   // helper function for has
+    if (!n) {
         return false;
     }
 
-    Card currentCard(n->suit, n->value);
-    Card targetCard(val1, val2);
-
-    if (currentCard < targetCard) {
-        return nodeFinder(val1, val2, n->right);
-    } else if (currentCard > targetCard) {
-        return nodeFinder(val1, val2, n->left);
+    if (val1 < n->suit || (val1 == n->suit && val2 < n->value)) {
+        return findNode(val1, val2, n->left);
+    } else if (val1 > n->suit || (val1 == n->suit && val2 > n->value)) {
+        return findNode(val1, val2, n->right);
     } else {
-        return true;
+        return true; // found
     }
 }
 
-vector<Card> CardBST::inOrderTraversal() {
-    vector<Card> result;
-    inOrderTraversal(root, result);
-    return result;
-}
-
-void CardBST::inOrderTraversal(Node* n, vector<Card>& result) {
-    if (n != nullptr) {
-        inOrderTraversal(n->left, result);
-        result.push_back(Card{n->suit, n->value});
-        inOrderTraversal(n->right, result);
+Node* CardBST::remove(int val1, int val2, Node* n) {    // remove helper function
+    if (!n) {
+        return nullptr;
     }
+
+    if (val1 < n->suit || (val1 == n->suit && val2 < n->value)) {
+        n->left = remove(val1, val2, n->left);
+    } else if (val1 > n->suit || (val1 == n->suit && val2 > n->value)) {
+        n->right = remove(val1, val2, n->right);
+    } else {
+        if (!n->left) {
+            Node* temp = n->right;
+            delete n;
+            return temp;
+        } else if (!n->right) {
+            Node* temp = n->left;
+            delete n;
+            return temp;
+        }
+
+        Node* temp = min(n->right);
+        n->suit = temp->suit;
+        n->value = temp->value;
+        n->right = remove(temp->suit, temp->value, n->right);
+    }
+
+    return n;
 }
-*/
--- main.cpp
+++ main.cpp
@@ -1,92 +1,10 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "cards.h"
-
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "cards.h"
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <alice_file> <bob_file>" << std::endl;
-        return 1;
-    }
-
-    // Read cards into binary search trees for Alice and Bob
-    BST aliceTree;
-    BST bobTree;
-
-    std::ifstream aliceFile(argv[1]);
-    std::ifstream bobFile(argv[2]);
-
-    if (!aliceFile || !bobFile) {
-        std::cerr << "Error: Could not open input files." << std::endl;
-        return 1;
-    }
-
-    readCards(aliceFile, aliceTree);
-    readCards(bobFile, bobTree);
-
-    aliceFile.close();
-    bobFile.close();
-
-    // Convert the binary search trees to vectors for efficient traversal
-    std::vector<Card> aliceRemaining = aliceTree.inOrderTraversal();
-    std::vector<Card> bobRemaining = bobTree.inOrderTraversal();
-
-    // Game loop
-    std::vector<std::pair<Card, Card>> matchingPairs;
-
-    while (!aliceRemaining.empty() && !bobRemaining.empty()) {
-        Card aliceCard = aliceRemaining.front();
-        Card bobCard = bobRemaining.front();
-
-        if (aliceCard == bobCard) {
-            // Matching pair found
-            matchingPairs.push_back({aliceCard, bobCard});
-
-            // Remove matching cards from both hands
-            aliceRemaining.erase(aliceRemaining.begin());
-            bobRemaining.erase(bobRemaining.begin());
-        } else if (aliceCard < bobCard) {
-            // Alice's card is smaller, remove from her hand
-            aliceRemaining.erase(aliceRemaining.begin());
-        } else {
-            // Bob's card is smaller, remove from his hand
-            bobRemaining.erase(bobRemaining.begin());
-        }
-    }
-
-    // Print matching pairs
-    for (const auto &pair : matchingPairs) {
-        std::cout << "Alice picked matching card " << pair.first << std::endl;
-        std::cout << "Bob picked matching card " << pair.second << std::endl;
-    }
-
-    // Print remaining cards for both players after the game ends
-    std::cout << "Alice's cards:" << std::endl;
-    for (const auto &c : aliceRemaining) {
-        std::cout << c << std::endl;
-    }
-
-    std::cout << "Bob's cards:" << std::endl;
-    for (const auto &c : bobRemaining) {
-        std::cout << c << std::endl;
-    }
-
-    return 0;
-}
-
-/*
 // main.cpp
 // Author: John Hagedorn
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <list>
+#include "utility.h"
 #include "cards.h"
 
 using namespace std;
@@ -117,29 +35,57 @@ int secondSet(string s2) {
     }
 }
 
-string vToString(int valueNum) {
-    if (valueNum == 1) {
-        return "a";
-    } else if (valueNum == 11) {
-        return "j";
-    } else if (valueNum == 12) {
-        return "q";
-    } else if (valueNum == 13) {
-        return "k";
-    } else {
-        return to_string(valueNum);
-    }
-}
+void playGame(CardBST& Alice, CardBST& Bob) {
+    Node* AliceNode = Alice.getMin();
+    Node* BobNode = Bob.getMax();
 
-string sToString(int suitValue) {
-    if (suitValue == 1) {
-        return "c";
-    } else if (suitValue == 2) {
-        return "d";
-    } else if (suitValue == 3) {
-        return "s";
-    } else {
-        return "h";
+    while (true) {
+        bool found = false;
+
+        if (AliceNode == nullptr || BobNode == nullptr) {
+            break;
+        }
+
+        // Alice's turn
+        while (AliceNode) {
+            if (Bob.find(AliceNode->suit, AliceNode->value)) {
+                found = true;
+                int suit = AliceNode->suit;
+                int value = AliceNode->value;
+                Alice.removeNode(suit, value);
+                Bob.removeNode(suit, value);
+                cout << "Alice picked matching card " << Alice.suitToString(suit) << " " << Alice.valToString(value) << endl;
+                AliceNode = Alice.succ(suit, value);
+                break;
+            } else {
+                AliceNode = Alice.succ(AliceNode->suit, AliceNode->value);
+            }
+        }
+
+        AliceNode = Alice.getMin();  // Reset AliceNode for the next iteration
+
+        // Bob's turn
+        while (BobNode) {
+            if (Alice.find(BobNode->suit, BobNode->value)) {
+                found = true;
+                int suit = BobNode->suit;
+                int value = BobNode->value;
+                Alice.removeNode(suit, value);
+                Bob.removeNode(suit, value);
+                cout << "Bob picked matching card " << Bob.suitToString(suit) << " " << Bob.valToString(value) << endl;
+                BobNode = Bob.pred(suit, value);
+                break;
+            } else {
+                BobNode = Bob.pred(BobNode->suit, BobNode->value);
+            }
+        }
+
+        BobNode = Bob.getMax();
+
+        // If no matching cards were found for both players, end loop
+        if (!found) {
+            break;
+        }
     }
 }
 
@@ -163,128 +109,26 @@ int main(int argc, char** argv) {
 
     // Read each file
     while (getline(cardFile1, line) && (line.length() > 0)) {
-        int a1;
-        int a2;
-        string strng1 = line.substr(0, 1);
-        string strng2;
-        if (line.length() == 4) {
-            strng2 = line.substr(2, 2);
-        } else {
-            strng2 = line.substr(2, 1);
-        }
-        a1 = firstSet(strng1);
-        a2 = secondSet(strng2);
+        int a1 = firstSet(line.substr(0, 1));
+        int a2 = secondSet(line.substr(2, (line.length() == 4) ? 2 : 1));
         Alice.insert(a1, a2);
     }
     cardFile1.close();
 
     while (getline(cardFile2, line) && (line.length() > 0)) {
-        int b1;
-        int b2;
-        string strng1 = line.substr(0, 1);
-        string strng2;
-        if (line.length() == 4) {
-            strng2 = line.substr(2, 2);
-        } else {
-            strng2 = line.substr(2, 1);
-        }
-        b1 = firstSet(strng1);
-        b2 = secondSet(strng2);
+        int b1 = firstSet(line.substr(0, 1));
+        int b2 = secondSet(line.substr(2, (line.length() == 4) ? 2 : 1));
         Bob.insert(b1, b2);
     }
     cardFile2.close();
 
-    Node* AliceNode = Alice.getMin();
-    Node* BobNode = Bob.getMax();
+    playGame(Alice, Bob);
 
-    while (true) {
-        bool aliceFound = false;
-        bool bobFound = false;
-
-        if (AliceNode == nullptr || BobNode == nullptr) {
-            break;
-        }
-
-        // Alice's turn
-        while (AliceNode) {
-            if (Bob.has(AliceNode->suit, AliceNode->value)) {
-                aliceFound = true;
-                int suit = AliceNode->suit;
-                int value = AliceNode->value;
-                Alice.removeNode(suit, value);
-                Bob.removeNode(suit, value);
-                cout << "Alice picked matching card " << sToString(suit) << " " << vToString(value) << endl;
-                AliceNode = Alice.successor(suit, value);
-                break;
-            } else {
-                AliceNode = Alice.successor(AliceNode->suit, AliceNode->value);
-            }
-        }
-
-        AliceNode = Alice.getMin();  // Reset AliceNode for the next iteration
-
-        // Bob's turn
-        while (BobNode) {
-            if (Alice.has(BobNode->suit, BobNode->value)) {
-                bobFound = true;
-                int suit = BobNode->suit;
-                int value = BobNode->value;
-                Alice.removeNode(suit, value);
-                Bob.removeNode(suit, value);
-                cout << "Bob picked matching card " << sToString(suit) << " " << vToString(value) << endl;
-                BobNode = Bob.predecessor(suit, value);
-                break;
-            } else {
-                BobNode = Bob.predecessor(BobNode->suit, BobNode->value);
-            }
-        }
-
-        BobNode = Bob.getMax();
-
-        // If no matching cards were found for both players, exit the loop
-        if (!aliceFound && !bobFound) {
-            break;
-        }
-    }
-
-    // Print remaining cards for Alice without duplicates
     cout << "Alice's cards:" << endl;
-    list<Card> aliceRemaining;
-    for (const auto& card : Alice.inOrderTraversal()) {
-        bool duplicate = false;
-        for (const auto& existingCard : aliceRemaining) {
-            if (existingCard == card) {
-                duplicate = true;
-                break;
-            }
-        }
-        if (!duplicate) {
-            aliceRemaining.push_back(card);
-        }
-    }
-    for (const auto& card : aliceRemaining) {
-        cout << sToString(card.suit) << " " << vToString(card.value) << endl;
-    }
+    Alice.printInOrder();
 
-    // Print remaining cards for Bob without duplicates
     cout << "Bob's cards:" << endl;
-    list<Card> bobRemaining;
-    for (const auto& card : Bob.inOrderTraversal()) {
-        bool duplicate = false;
-        for (const auto& existingCard : bobRemaining) {
-            if (existingCard == card) {
-                duplicate = true;
-                break;
-            }
-        }
-        if (!duplicate) {
-            bobRemaining.push_back(card);
-        }
-    }
-    for (const auto& card : bobRemaining) {
-        cout << sToString(card.suit) << " " << vToString(card.value) << endl;
-    }
+    Bob.printInOrder();
 
     return 0;
 }
-*/
