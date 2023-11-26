37645437627ffd014a7fcdaedbbb51a9e4d207dc
johnhagedorncs <johnhagedorn@ucsb.edu>
2023-11-26 11:49:55 -0800

    bruh

--- cards.cpp
+++ cards.cpp
@@ -77,13 +77,12 @@ Node* CardBST::pred(int val1, int val2) {
 
 void CardBST::printInOrder() {  // printInOrder function
     printInOrder(root);
-    cout << endl;
 }
 
 void CardBST::printInOrder(Node* n) {   // printInOrder helper function
     if (n) {
         printInOrder(n->left);
-        cout << suitToString(n->suit) << " " << valToString(n->value) << " ";
+        cout << suitToString(n->suit) << " " << valToString(n->value) << endl;
         printInOrder(n->right);
     }
 }
